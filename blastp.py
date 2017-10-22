35543104ea58f1f3f04d2e0423f0b6a4f9b5f4be
LorenaDerezanin <lorke@github.com>
2017-10-22 17:24:29 +0200

    comment added

--- blastp.py
+++ blastp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# written by Lorena Derezanin 
+# run multiple blastp runs with each protein seq file in dir as a query
 
 import argparse
 import os
@@ -26,6 +26,7 @@ protein_files = os.listdir(path)
 # os.listdir returns list of file names as strings
 for protein_file in protein_files:
     f_out_path = os.path.join(args.output, prot_name(protein_file) + "_blastp")
+    # f_out_path contains output file path as string
     # os.path.join concatenates strings with "/"
     f_in_path = os.path.join(path, protein_file)
     blast = "blastp -query %s -out %s -db %s -evalue %s -word_size %s" % (f_in_path, f_out_path, args.database, args.evalue, args.word_size)
