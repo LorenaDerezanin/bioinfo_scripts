d4077f2f5b36ad0a8397e33a1d3b88de980cdb11
LorenaDerezanin <lorke@github.com>
2017-10-23 11:16:21 +0200

    query and target added as arguments, input deleted

--- multi_exo.py
+++ multi_exo.py
@@ -7,7 +7,8 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-i", "--input", dest="input", action="store", required=True)
+parser.add_argument("-q", "--query", dest="query", action="store", required=True)
+parser.add_argument("-t", "--target", dest="target", action="store", required=True)
 parser.add_argument("-o", "--output", dest="output", action="store", required=True)
 parser.add_argument("-n", "--bestn", dest="bestn", action="store", type=int)
 parser.add_argument("-s", "--score", dest="score", action="store", type=int, default=100)
@@ -23,14 +24,15 @@ def gene_name(protein_name):
     return split_name[0]
 
 
-path = args.input
+path = args.query
 
 protein_files = os.listdir(path)
 for protein_file in protein_files:
     f_out_path = os.path.join(args.output, gene_name(protein_file) + "_exo")
     f_in_path = os.path.join(path, protein_file)
     exo = "exonerate -m protein2genome -q %s -t %s --bestn %s --dpmemory %s -s %s -V 3 -Q protein -T dna " \
-          "--showalignment yes --showtargetgff yes" % (f_in_path, f_out_path, args.bestn, args.dpm, args.score)
+          "--showalignment yes --showtargetgff yes > -o %s" % (f_in_path, args.target, args.bestn, args.dpm,
+                                                               args.score, f_out_path)
     os.system(exo)
 
 
