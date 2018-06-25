76ec63166ebd820fb1dea46713bc114d9671ac89
LorenaDerezanin <lorenaderezanin@gmail.com>
2018-06-25 15:33:13 +0200

    progress report path changed

--- gene_mining_18-21.sh
+++ gene_mining_18-21.sh
@@ -22,7 +22,7 @@ cd SINHITS
 mkdir FASTA
 mv *_fasta FASTA
 echo "13) Singleton-sequences extracted" \
-	>> ~/Progressreports_2015/Progress_$FISH
+	>> $PROGRESS_REPORTS
 
 ##GET EACH SINGLETON TO SEPARATE FILES BEFORE ORF PREDICTION
 for f in FASTA/*; do \
@@ -71,26 +71,26 @@ cd SIN_ORFs
 mkdir ORFs_FASTA
 mv *_ORFs ORFs_FASTA
 echo "14) Singleton-ORFs predicted and parsed" \
-	>> ~/Progressreports_2015/Progress_$FISH
+	>> $PROGRESS_REPORTS
 
 
 ##ALIGN SINGLETONS TO UNIPROT DATABASE
 for f in ORFs_FASTA/*; do \
-        blastp \
-        -query $f \
-        -db $WORKING_DIR_PATH/gadus_CA/9-terminator/DATABASES/BLASTS/UTG/merged_uniprotdbs_nospace \
-        -out ${f%_merged_ORFs}"_reciprocal_sin_hits" \
-        -evalue 1 \
-        -outfmt 6 \
-        -max_target_seqs 20 \
-        -num_threads 16 \
-        ; done
+    blastp \
+    -query $f \
+    -db $WORKING_DIR_PATH/gadus_CA/9-terminator/DATABASES/BLASTS/UTG/merged_uniprotdbs_nospace \
+    -out ${f%_merged_ORFs}"_reciprocal_sin_hits" \
+    -evalue 1 \
+    -outfmt 6 \
+    -max_target_seqs 20 \
+    -num_threads 16 \
+    ; done
 
 cd ORFs_FASTA
 mkdir RECIPROCAL_HITS
 mv *_hits RECIPROCAL_HITS
 echo "15) Predicted Singleton-ORFs aligned to UniProt database" \
-	>> ~/Progressreports_2015/Progress_$FISH
+	>> $PROGRESS_REPORTS
 
 ##DETERMINE CONFIRMED HITS AND GET ANNOTATION
 for f in RECIPROCAL_HITS/*; do \
@@ -126,11 +126,11 @@ for f in ONELINERS/*_beautified; do \
 	>> List_of_genes_found_in_singletons \
 	; done
 echo "16) Genes present in singletons, based on reciprocal hits (e-value < 1e-1):" \
-	>> ~/Progressreports_2015/Progress_$FISH
+	>> $PROGRESS_REPORTS
 for f in ONELINERS/*_beautified; do \
 	head $f | \
 	awk '/=/{print$0}' \
-	>> ~/Progressreports_2015/Progress_$FISH \
+	>> $PROGRESS_REPORTS \
 	; done
 
 ##REPORT WHICH GENES ARE ABSENT 
@@ -146,6 +146,6 @@ grep \
 	~/Gene_names_only \
 	> Genes_absent
 echo "17) These genes are NOT present in the genome:" \
-	>> ~/Progressreports_2015/Progress_$FISH
+	>> $PROGRESS_REPORTS
 cat Genes_absent \
->> ~/Progressreports_2015/Progress_$FISH
+>> $PROGRESS_REPORTS
