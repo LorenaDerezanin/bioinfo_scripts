c91732c8433b2ff0ee0753ac57a020055587ee5c
LorenaDerezanin <lorenaderezanin@gmail.com>
2018-08-14 15:47:53 +0200

    added duration

--- busco_assembly_check.sh
+++ busco_assembly_check.sh
@@ -1,10 +1,22 @@
-#!/bin/bash
 
 # example of a genome assembly quality check up
 # BUSCO version: BUSCO 3.0.1
-PATH="/home/derezanin/species_comp/wolverine"
 
-time run_BUSCO -i $PATH/CYRY01.fasta \
--o wolverine_busco -l $PATH/mammalia_odb9 \
+# run it directly in the command line, not as a script
+
+time run_BUSCO -i /home/derezanin/species_comp/wolverine/CYRY01.fasta \
+-o wolverine_busco -l /home/derezanin/species_comp/wolverine/mammalia_odb9 \
 -m geno -c 16 \
 2> busco_wolverine.log
+
+# time: 768 min (14.8.18)
+
+
+time tblastn -query /home/derezanin/species_comp/atlantic_cod/fish_paired_reads/gadus_3_assembly/e-value_cutoff_check/MHC-U_NCBI.faa \
+-db /home/derezanin/species_comp/atlantic_cod/fish_paired_reads/gadus_3_assembly/e-value_cutoff_check/ref_g_50.fasta -out ref_g50_e5.out \
+-evalue 1e-5 -outfmt 6 \
+-num_threads 16 \
+2> ref_g50_e5.err
+
+
+-max_target_seqs 10 \
